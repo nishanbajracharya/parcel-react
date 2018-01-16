9ab269ef756ebf05cd2df0ac5c067828d839a353
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2018-01-16 10:23:34 +0545

    Add eslint and lint src files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react';
+import { render } from 'react-dom';
 
 import 'normalize.css';
 
 const App = () => <h1>Setting up React with Parcel bundler</h1>;
 
-const wrapper = document.getElementById("app");
+const wrapper = document.getElementById('app');
 render(<App />, wrapper);
